ac9fa5d8c57bb06430b1843583752969e2c241cd
Dominik Hladík <dominik.hladik@seznam.cz>
2021-05-30 09:21:29 +0200

    feat(fixed-footer): make directive working

--- projects/demo/src/app/app.component.ts
+++ projects/demo/src/app/app.component.ts
@@ -9,4 +9,36 @@ import { VERSION } from '../environments/version';
 export class AppComponent {
   public title = 'ngx-fixed-footer';
   public version = VERSION;
+  public readonly Array = Array;
+  public loremIpsum =
+    'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Etiam posuere lacus quis dolor. Cras pede libero, dapibus nec, pretium sit amet, tempor quis. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Fusce tellus. Duis viverra diam non justo. In enim a arcu imperdiet malesuada. Cras elementum. Fusce wisi. Mauris metus. Fusce aliquam vestibulum ipsum. Aliquam erat volutpat.';
+
+  public showFooter = true;
+  public displayFooter = true;
+  public footerCssAttribute: 'padding' | 'margin' = 'padding';
+  public containerSelector = '[role="main"]';
+
+  public toggleIf(): void {
+    this.showFooter = !this.showFooter;
+  }
+
+  public toggleDisplay(): void {
+    this.displayFooter = !this.displayFooter;
+  }
+
+  public toggleFooterCssAttribute(): void {
+    if (this.footerCssAttribute === 'padding') {
+      this.footerCssAttribute = 'margin';
+    } else {
+      this.footerCssAttribute = 'padding';
+    }
+  }
+
+  public toggleCssSelector(): void {
+    if (this.containerSelector === '[role="main"]') {
+      this.containerSelector = '#test';
+    } else {
+      this.containerSelector = '[role="main"]';
+    }
+  }
 }
--- projects/ngx-fixed-footer/src/lib/ngx-fixed-footer.directive.ts
+++ projects/ngx-fixed-footer/src/lib/ngx-fixed-footer.directive.ts
@@ -1,6 +1,80 @@
-import { Directive } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import {
+  Directive,
+  DoCheck,
+  ElementRef,
+  Inject,
+  Input,
+  OnChanges,
+  OnDestroy,
+  Renderer2,
+  SimpleChanges
+} from '@angular/core';
 
 @Directive({
-  selector: '[NgxFixedFooter]'
+  selector: '[ngxFixedFooter]'
 })
-export class NgxFixedFooterDirective {}
+export class NgxFixedFooterDirective implements DoCheck, OnDestroy, OnChanges {
+  @Input() public containerSelector: string = '[role="main"]';
+  @Input() public cssAttribute: 'padding' | 'margin' = 'padding';
+
+  private offsetHeight: number = undefined;
+
+  constructor(private el: ElementRef, private render: Renderer2, @Inject(DOCUMENT) private document: Document) {}
+
+  public ngOnChanges(changes: SimpleChanges): void {
+    console.log(changes);
+    // swap selector
+    if (changes?.containerSelector && !changes?.containerSelector?.firstChange) {
+      if (changes?.containerSelector?.currentValue !== changes?.containerSelector?.previousValue) {
+        const prev = changes?.containerSelector?.previousValue;
+        const next = changes?.containerSelector?.currentValue;
+        this.removeStyle(this.document.body.querySelector(prev), this.cssAttribute);
+        this.setStyle(this.document.body.querySelector(next), this.cssAttribute, this.offsetHeight);
+      }
+    }
+
+    // swap margin and padding
+    if (changes?.cssAttribute && !changes?.cssAttribute?.firstChange) {
+      if (changes?.cssAttribute?.currentValue === 'margin') {
+        const container = this.container;
+        this.removeStyle(container, 'padding');
+        this.setStyle(container, 'margin', this.offsetHeight);
+      } else if (changes?.cssAttribute?.currentValue === 'padding') {
+        const container = this.container;
+        this.removeStyle(container, 'margin');
+        this.setStyle(container, 'padding', this.offsetHeight);
+      }
+    }
+  }
+
+  public ngOnDestroy(): void {
+    this.removeStyle(this.container, this.cssAttribute);
+  }
+
+  public ngDoCheck(): void {
+    setTimeout(() => {
+      const height = this.html.offsetHeight;
+      if (this.offsetHeight != height) {
+        this.setStyle(this.container, this.cssAttribute, height);
+        this.offsetHeight = height;
+      }
+    }, 0);
+  }
+
+  private removeStyle(container: HTMLElement, cssAttribute: 'padding' | 'margin'): void {
+    this.render.setStyle(container, `${cssAttribute}-bottom`, '');
+  }
+
+  private setStyle(container: HTMLElement, cssAttribute: 'padding' | 'margin', height: number): void {
+    this.render.setStyle(container, `${cssAttribute}-bottom`, height === 0 ? '' : `${height}px`);
+  }
+
+  private get container(): HTMLElement {
+    return this.document.body.querySelector(this.containerSelector);
+  }
+
+  private get html(): HTMLElement {
+    return this.el.nativeElement;
+  }
+}
