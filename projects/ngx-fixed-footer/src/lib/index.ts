3e88ca3890e98852ad695e1c10dc550be682e75f
Dominik Hladík <dominik.hladik@seznam.cz>
2021-05-30 11:23:02 +0200

    fix(fixed-footer): export api

--- projects/ngx-fixed-footer/src/lib/index.ts
+++ projects/ngx-fixed-footer/src/lib/index.ts
@@ -1,2 +1,6 @@
+export * from './ngx-fixed-footer-factory';
+export * from './ngx-fixed-footer-options.service';
+export * from './ngx-fixed-footer.constants';
 export * from './ngx-fixed-footer.directive';
+export * from './ngx-fixed-footer.interface';
 export * from './ngx-fixed-footer.module';
--- projects/ngx-fixed-footer/src/public-api.ts
+++ projects/ngx-fixed-footer/src/public-api.ts
@@ -2,5 +2,8 @@
  * Public API Surface of ngx-fixed-footer
  */
 
+export * from './lib/ngx-fixed-footer-options.service';
+export * from './lib/ngx-fixed-footer.constants';
 export * from './lib/ngx-fixed-footer.directive';
+export * from './lib/ngx-fixed-footer.interface';
 export * from './lib/ngx-fixed-footer.module';
