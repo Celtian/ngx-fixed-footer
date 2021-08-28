a6a9742dce7857cd0ad1647add1f96658bf25248
Dominik Hladík <dominik.hladik@seznam.cz>
2021-08-28 13:35:43 +0200

    chore(deps): angular updated

--- projects/demo/src/environments/version.ts
+++ projects/demo/src/environments/version.ts
@@ -1,7 +1,7 @@
 // IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
 /* tslint:disable */
 export const VERSION = {
-  TAG: '0.0.0',
-  COMMIT_HASH: ''
+    "TAG": "0.0.2",
+    "COMMIT_HASH": ""
 };
 /* tslint:enable */
