19bc60fd812147cb00927b5269c712040d0f84ab
Dominik Hladík <dominik.hladik@seznam.cz>
2023-08-15 23:19:01 +0200

    fix(tests): fix tests for standalone

--- projects/demo/src/app/app.component.spec.ts
+++ projects/demo/src/app/app.component.spec.ts
@@ -8,11 +8,8 @@ describe('AppComponent', () => {
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
       imports: [
-        NgxFixedFooterModule
+        AppComponent, NgxFixedFooterModule
       ]
     }).compileComponents();
 
